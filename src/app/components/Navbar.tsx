f9dafb5faf29d3d637ff7d5127dc23bfdd66ba95
Nicholas_Miyasato <nicholetas@gmail.com>
2023-05-10 00:00:50 -0400

    improving

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,28 +1,33 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import style from './Navbar.module.css'
 
 export default function Navbar(){
     return(
         <>
         <nav className="
-        bg-blue-900/10
+        sticky
+        bg-clip-padding
+        bg-blue-950/70
+        top-0
+        flex
+        justify-between
+        items-center
+        blur-backdrop-filter
         p-5
-
         md:flex md:items-center md:justify-between">
             <div>
                 <Link href='/'>
                     <Image 
-                    className='ml-10 hover:w-36 cursor-pointer hover:opacity-80 duration-300' 
+                    className='ml-10 hover:w-36 cursor-pointer duration-300' 
                     src='https://www.famsecurity.com.br/wp-content/uploads/logotipo-1.png' alt='' 
                     width={130} height={100} />
                 </Link>
             </div>
             <ul className='flex row text-2xl font-normal space-x-10'>
-                <li><Link className=' hover:text-sky-900 hover:font-bold duration-300' href="/">Home</Link></li>
-                <li><Link className=' hover:text-yellow-400 hover:font-bold duration-300' href="/about">About Us</Link></li>
-                <li><Link className=' hover:text-amber-500 hover:font-bold duration-300' href="/services">Services</Link></li>
-                <li><Link className=' hover:text-green-600 hover:font-bold duration-300' href="/contact">Contact Us</Link></li>
+                <li><Link className=' hover:text-sky-900 hover:font-bold duration-300 ease-linear' href="/">Home</Link></li>
+                <li><Link className=' hover:text-yellow-400 hover:font-bold duration-300 ease-linear' href="/about">About Us</Link></li>
+                <li><Link className=' hover:text-amber-500 hover:font-bold duration-300 ease-linear' href="/services">Services</Link></li>
+                <li><Link className=' hover:text-green-600 hover:font-bold duration-300 ease-linear' href="/contact">Contact Us</Link></li>
             </ul>
         </nav>
         </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,27 @@
-import Image from 'next/image'
-import Link from 'next/link'
 import Navbar from './components/Navbar'
-import style from '../styles/Home.module.css'
+import Image from 'next/image'
+import facilities from '../public/icons/facilities.svg'
+import { ReactComponent as MySvg } from './my-svg-file.svg';
+
 
 export default function Home() {
   return (
     <>
     <Navbar/>
-    <div className='bg-blue-900/50 text-9xl h-screen'>
-      <h1>Home</h1>
+    <div className='
+    flex flex-col md:flex-row
+    bg-gradient-to-r
+    from-black
+    to-blue-900
+    background-animate text-slate-400'>
+      <div className='min-w-[50%] flex flex-col justify-center items-center text-center p-6'>
+        <h1 className='text-4xl text-white font-black uppercase'>nós não brincamos com</h1>
+        <h1 className='text-4xl font-black gradient-text uppercase'>coisa séria</h1>
+        <p className='text-lg font-bold text-gray3 uppercase'>Aqui nossa prioridade é trazer o melhor de segurança que o mercado pode oferecer direto para o seu negócio</p>
+      </div>
+      <div className='min-w-[50%] flex flex-col justify-center items-center text-center p-6 h-96'>
+        <Image src={facilities} alt='' width={300} height={300} />
+        </div>
     </div>
     </>
   )
