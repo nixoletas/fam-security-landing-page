4db5aeb8899faff65d8ab4d840e2e916141f4568
Nicholas_Miyasato <nicholetas@gmail.com>
2023-05-10 15:34:02 -0400

    forgot to deactivate my dark reader
    now i have to change all the theme again

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,14 +7,15 @@ export default function Navbar(){
         <nav className="
         sticky
         bg-clip-padding
-        bg-blue-950/70
+        bg-blue-950
         top-0
         flex
         justify-between
         items-center
         blur-backdrop-filter
-        p-5
+        p-3
         md:flex md:items-center md:justify-between">
+            
             <div>
                 <Link href='/'>
                     <Image 
@@ -24,10 +25,10 @@ export default function Navbar(){
                 </Link>
             </div>
             <ul className='flex row text-2xl font-normal space-x-10'>
-                <li><Link className=' hover:text-sky-900 hover:font-bold duration-300 ease-linear' href="/">Home</Link></li>
-                <li><Link className=' hover:text-yellow-400 hover:font-bold duration-300 ease-linear' href="/about">About Us</Link></li>
-                <li><Link className=' hover:text-amber-500 hover:font-bold duration-300 ease-linear' href="/services">Services</Link></li>
-                <li><Link className=' hover:text-green-600 hover:font-bold duration-300 ease-linear' href="/contact">Contact Us</Link></li>
+                <li><Link className=' shadow-cyan-500 hover:shadow-2xl hover:text-sky-900 hover:font-black duration-300 ease-linear' href="/">Home</Link></li>
+                <li><Link className=' hover:text-yellow-400 hover:font-black duration-300 ease-linear' href="/about">About Us</Link></li>
+                <li><Link className=' hover:text-amber-500 hover:font-black duration-300 ease-linear' href="/services">Services</Link></li>
+                <li><Link className=' hover:text-green-600 hover:font-black duration-300 ease-linear' href="/contact">Contact Us</Link></li>
             </ul>
         </nav>
         </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,38 @@
+import Link from 'next/link'
 import Navbar from './components/Navbar'
 import Image from 'next/image'
-import facilities from '../public/icons/facilities.svg'
-import { ReactComponent as MySvg } from './my-svg-file.svg';
-
 
 export default function Home() {
   return (
     <>
     <Navbar/>
+    <div className='bg-gradient-to-r from-blue-950/70 to-blue-950/30 text-9xl text-slate-400 h-screen'>
     <div className='
-    flex flex-col md:flex-row
-    bg-gradient-to-r
-    from-black
-    to-blue-900
-    background-animate text-slate-400'>
-      <div className='min-w-[50%] flex flex-col justify-center items-center text-center p-6'>
-        <h1 className='text-4xl text-white font-black uppercase'>nós não brincamos com</h1>
-        <h1 className='text-4xl font-black gradient-text uppercase'>coisa séria</h1>
+    flex flex-col transition-all md:flex-row
+    bg-blue-950/50
+    background-animate text-slate-400 h-full'>
+      <div className='min-w-[50%] mt-24 flex flex-col items-center text-center'>
+        <h1 className='text-4xl p-2 text-white font-black uppercase'>segurança é 
+        <span className='text-4xl font-black gradient-text uppercase'> coisa séria!</span></h1>
         <p className='text-lg font-bold text-gray3 uppercase'>Aqui nossa prioridade é trazer o melhor de segurança que o mercado pode oferecer direto para o seu negócio</p>
+        <button className='shadow-cyan-500/50 flex flex-col bg-slate-950 hover:bg-slate-900 text-white font-bold uppercase p-4 rounded-md hover:rounded-xl mt-4 text-sm hover:text-base duration-300 ease-in-out'>SAIBA MAIS</button>
       </div>
       <div className='min-w-[50%] flex flex-col justify-center items-center text-center p-6 h-96'>
-        <Image src={facilities} alt='' width={300} height={300} />
+        <div className='flex flex-row space-x-10'>
+          <div className='relative'>
+        <Image className='shadow-cyan-500/50 hover:w-52 duration-500 ease-in-out'src="/house.svg" alt="My SVG file" width={170} height={170}/>
+          </div>
+        <Image className='hover:w-52 duration-500 ease-in-out' src="/technology.svg" alt="My SVG file" width={170} height={170}/>
+        <Image className='hover:w-52 hover:dark:invert-3 duration-500 ease-in-out' src="/vigilant.svg" alt="My SVG file" width={170} height={170}/>
         </div>
+      </div>
+    </div>
+    <div className='m-40 text-center space-y-96 h-96 text-7xl uppercase font-black'>
+      <h1>Tecnologia</h1>
+      <h1>Segurança</h1>
+      <h1>Qualidade</h1>
+      <h1>Serviços e facilities</h1>
+    </div>
     </div>
     </>
   )
