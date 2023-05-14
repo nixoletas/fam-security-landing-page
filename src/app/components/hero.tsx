9722186d1d11a4dfe9af78962f83c3ab901b8400
Nicholas_Miyasato <nicholetas@gmail.com>
2023-05-13 21:30:46 -0400

    Update hero.tsx

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -11,11 +11,11 @@ export default function Hero() {
      text-slate-400'>
         <div className='animate-fade-in-down mt-2  flex flex-col 
         items-center text-center'>
-          <h1 className='px-12 text-3xl text-white font-black uppercase'>
+          <h1 className='px-12 text-3xl max-sm:text-2xl text-white font-black uppercase'>
             <span className=''>vigilância</span>,
             <span className='font-FiraCode'> tecnologia </span>
             <span className='font-trebuchet '>e facilities </span><br/>
-          <span className='text-3xl gradient-light font-bold uppercase'> na palma da sua mão!</span></h1>
+          <span className='max-sm:text-2xl text-3xl gradient-light font-bold uppercase'> na palma da sua mão!</span></h1>
           <p className='text-lg font-bold font-sans  text-gray3 uppercase'>segurança, 
           parceria e confiança.</p>
           <a href='#presentation'>
@@ -27,7 +27,7 @@ export default function Hero() {
 
         <div className=' flex flex-col my-10 mx-1 justify-center items-center text-center'>
           <div className='animate-fade-in-up flex flex-row space-x-10'>
-          <Image className='max-sm:w-20  shadow-cyan-500/50  hover:animate-pulse 
+          <Image className='max-sm:w-20  shadow-cyan-500/50   
           dark:drop-shadow-[0_0_0.3rem_#ffffff] duration-500 ease-in-out'
           src="/vigilant.svg" alt="My SVG file" width={120} height={120}/>
           <Image className=' max-sm:w-20 duration-500 hover:animate-pulse
@@ -40,7 +40,7 @@ export default function Hero() {
         </div>
 
     </div>
-        <div className=' my-28 dark:drop-shadow-[0_0_0.1rem_#ffffff70] w-full flex animate-bounce align-bottom  justify-center text-center '>
+        <div className=' mt-28 dark:drop-shadow-[0_0_0.1rem_#ffffff70] w-full flex animate-bounce align-bottom  justify-center text-center '>
             <Image src='/scroll-down.svg' alt='scroll down' width={50} height={50}/>
         </div>
     </div>
