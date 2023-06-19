07122093af77020345372e41cfbe787aa78e70d0
Nicholas_Miyasato <66659340+nixoletas@users.noreply.github.com>
2023-06-19 15:42:21 +0000

    arrow-2

--- src/app/components/Arrow.tsx
+++ src/app/components/Arrow.tsx
@@ -3,11 +3,9 @@ import Image from 'next/image';
 
 export default function Arrow() {
   return (
-    <div className="animate-bounce" style={{
+    <div className="flex justify-center animate-bounce w-full" style={{
         position: 'fixed',
-        left: '50%',
         bottom: '0',
-        transform: 'translateX(-50%)',
       }}
     >
       <Image src="/arrow-down.svg" alt="My SVG file" width={60} height={30} />
