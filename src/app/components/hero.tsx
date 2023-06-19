d9a71d05417d3f909d90abe1bb44060010554b94
Nicholas_Miyasato <66659340+nixoletas@users.noreply.github.com>
2023-06-19 15:09:29 +0000

    style adjust background animation

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -13,8 +13,8 @@ export default function Hero() {
         items-center text-center'>
           <h1 className='text-3xl max-sm:text-2xl text-white font-black uppercase'>
             <span className=''>vigilância</span>,
-            <span className='font-FiraCode'> tecnologia </span>
-            <span className='font-trebuchet '>e facilities </span><br/>
+            <span className=''> tecnologia </span>
+            <span className=''>e facilities </span><br/>
           <span className='max-sm:text-2xl text-3xl gradient-light font-bold uppercase'> na palma da sua mão!</span></h1>
           <p className='text-lg font-bold font-sans  text-gray3 uppercase'>segurança, 
           parceria e confiança.</p>
@@ -28,13 +28,13 @@ export default function Hero() {
         <div className=' flex flex-col my-10 mx-1 justify-center items-center text-center'>
           <div className='flex flex-row space-x-10'>
           <Image className='max-sm:w-24  shadow-cyan-500/50   
-          dark:drop-shadow-[0_0_0.3rem_#ffffff]'
+          dark:drop-shadow-[0_0_0.3rem_#ffffff] hover:scale-110 duration-300 ease-in-out'
           src="/vigilant.svg" alt="My SVG file" width={120} height={120}/>
           <Image className=' max-sm:w-20 
-          dark:drop-shadow-[0_0_0.3rem_#ffffff] ' 
+          dark:drop-shadow-[0_0_0.3rem_#ffffff] hover:scale-110 duration-300 ease-in-out' 
           src="/technology.svg" alt="My SVG file" width={120} height={120}/>
           <Image className='max-sm:w-20 
-          dark:drop-shadow-[0_0_0.3rem_#ffffff]' 
+          dark:drop-shadow-[0_0_0.3rem_#ffffff] hover:scale-110 duration-300 ease-in-out' 
           src="/facilities.svg" alt="My SVG file" width={120} height={120}/>
           </div>
         </div>
