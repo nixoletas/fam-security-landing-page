1897e662ecb0ec4889bb216174a6bd4fc1255447
Nicholas_Miyasato <nicholetas@gmail.com>
2023-09-21 15:20:06 -0400

    a little improvement

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,11 +1,19 @@
+'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Navbar(){
-    return(
-        <>
-        <nav className="
+export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <>
+      <nav className="
         bg-black/50
         w-full
         top-0
@@ -15,35 +23,34 @@ export default function Navbar(){
         items-center
         blur-backdrop-filter
         p-2 fixed">
-            <div>
-                <Link href='/'>
-                <Image
-                className='ml-2 cursor-pointer duration-300
-                dark:drop-shadow-[0_0_0.4rem_#000000] hover:scale-110' 
-                src='https://www.famsecurity.com.br/wp-content/uploads/logotipo-1.png' alt='' 
-                width={120} height={95} priority={true} />
-                </Link>
-            </div>
-            <ul className=' max-md:hidden flex row text-2xl font-extralight mr-5 space-x-10'>
-                <li className=' text-white hover:text-blue-600 hover:font-black 
-                duration-200 ease-in-out'>
-                    <Link href='/' >Home</Link>
-                </li>
-                <li className=' text-white hover:text-yellow-400 hover:font-black 
-                duration-200 ease-in-out'>
-                    <Link href='/about'>About Us</Link>
-                </li>
-                <li className=' text-white hover:text-amber-500 hover:font-black 
-                duration-200 ease-out'>
-                    <Link href='/contact'>Contact Us</Link>
-                </li>
-                <li className=' text-white hover:text-green-600 hover:font-black 
-                duration-200 ease-in-out'>
-                    <Link href='/services'>Services</Link>
-                </li>
-            </ul>
-        </nav>
-        </>
-    )
+        <div>
+          <Link href='/'>
+            <Image
+              className='ml-2 cursor-pointer duration-300 dark:drop-shadow-[0_0_0.4rem_#000000] hover:scale-110'
+              src='https://www.famsecurity.com.br/wp-content/uploads/logotipo-1.png' alt=''
+              width={120} height={95} priority={true} />
+          </Link>
+        </div>
+        <div className='min-md:hidden'>
+          <button onClick={toggleMenu} className='text-white text-3xl p-2 focus:outline-none'>
+            ☰
+          </button>
+        </div>
+        <ul className={`min-md:flex ${menuOpen ? 'block' : 'hidden'} text-lg`}>
+          <li className=' text-white hover:text-blue-600 hover:font-black duration-200 ease-in-out'>
+            <Link href='/' >Home</Link>
+          </li>
+          <li className=' text-white hover:text-yellow-400 hover:font-black duration-200 ease-in-out'>
+            <Link href='/about'>About Us</Link>
+          </li>
+          <li className=' text-white hover:text-amber-500 hover:font-black duration-200 ease-out'>
+            <Link href='/contact'>Contact Us</Link>
+          </li>
+          <li className=' text-white hover:text-green-600 hover:font-black duration-200 ease-in-out'>
+            <Link href='/services'>Services</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
 }
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import Arrow from './components/Arrow'
 import Section from './components/Section'
 
 export default function Home() {
