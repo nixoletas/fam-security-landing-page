9d9eb86684671d00244663a4dc8ee300213cf009
Nicholas_Miyasato <nicholetas@gmail.com>
2023-09-27 14:21:17 -0400

    Update Section.tsx

--- src/app/components/Section.tsx
+++ src/app/components/Section.tsx
@@ -1,22 +1,6 @@
-'use client'
-
 import React from 'react';
 
 export default function Section(){
-
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('show');
-            }
-        })
-    })
-    
-    const hiddenElements = document.querySelectorAll('.hided');
-    hiddenElements.forEach((el) => observer.observe(el));
-    
-    
-    
     return(
         <>
             <div className="h-full w-screen items-center text-slate-50 flex justify-center flex-col text-center">
@@ -27,7 +11,7 @@ export default function Section(){
                     <i className="gradient-lightArrow text-6xl pb-32 animate-bounce fa-solid fa-arrow-down"></i>
                 </div>
 
-                <div className='hided'>
+                <div>
                     <p className="flex flex-col mx-1 pb-24 sm:text-4xl font-FiraCode font-semibold text-4xl">
                     Garantimos a continuidade <br></br> 
                     da atividade fim.<br></br>
